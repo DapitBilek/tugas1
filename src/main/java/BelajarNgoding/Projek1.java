d4ed190142879fcae6d905154b8787f5582a4cf2
DapitBilek <davidarifqisyahputra@gmail.com>
2022-04-05 12:01:53 +0700

    first commit

---
+++ src/main/java/BelajarNgoding/Projek1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package BelajarNgoding;
+
+/**
+ *
+ * @author Acer
+ */
+
+class Player{
+    String name;
+    String pasif;
+    String ultimate;
+}
+public class Projek1 {
+    public static void main(String[] args) {
+        Player player1 = new Player();
+        player1.name = "Dapit";
+        player1.pasif = "Invisible";
+        player1.ultimate = "Blazingduet";
+        System.out.println(player1.name);
+        System.out.println(player1.pasif);
+        System.out.println(player1.ultimate);
+    }
+}
